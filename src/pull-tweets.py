399a31a1a3f84e32df651d65cfbfb6a6e51ed22e
Apostolos-Delis <apostolos.delis@yahoo.com>
2018-09-08 17:23:27 -0700

    ADDED a multithreaded approach to pulling data from many different hashtags

--- src/pull-tweets.py
+++ src/pull-tweets.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # coding: utf8
 
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 import time
 import os
 import json
 import pprint
+import threading 
+import time
+from queue import Queue
+
 from json_parser import JsonTweetParser
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+from constants import JSON_DIR, TWEET_DIR, DATA_DIR
 
 # Variables that contains the user credentials to access Twitter API
 from api_keys import ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET 
@@ -25,66 +31,104 @@ try:
 except ImportError:
     import simplejson as json
 
-# Import the necessary methods from "twitter" library
+#Hashtags that will be searched for using the twitter api
+hashtags = [ "bitcoin", "ethereum", "litecoin", "ripple", "bcash", "eos",
+           "stellarlumens", "monero", "nano", "vechain"]
 
-oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+NUM_THREADS = len(hashtags)
 
-# Initiate the connection to Twitter Streaming API
-twitter_stream = TwitterStream(auth=oauth)
-
-# Get a sample of the public data following through Twitter
-#iterator = twitter_stream.statuses.sample()
-
-# Print each tweet in the stream to the screen
-# Here we set it to stop after getting 1000 tweets.
-# You don't have to set it to stop, but can continue running
-# the Twitter API to collect data for days or even longer.
-# tweet_count = 1000
-# for tweet in iterator:
-#     tweet_count -= 1
-#     # Twitter Python Tool wraps the data returned by Twitter
-#     # as a TwitterDictResponse object.
-#     # We convert it back to the JSON format to print/score
-#     print(json.dumps(tweet))
-#
-#     # The command below will do pretty printing for JSON data, try it out
-#     # print json.dumps(tweet, indent=4)
-#
-#     if tweet_count <= 0:
-#         break
+SECONDS_PER_ITERATION = 15
 
 twitter = Twitter(auth=oauth)
 
-hashtags = [ "bitcoin", "ethereum", "litecoin", "ripple", "bcash", "eos", "stellarlumens",
-        "monero", "nano", "vechain"]
-
 print("Beginning to pull data...")
-count = 0
-while True:
 
-    timestr = time.strftime("%Y-%m-%d_%H-%M-%S")
-    current_path = "json_files"
-    file_name = os.path.join(current_path, "btc_" + str(count) + "_" + str(timestr) + ".json")
+print_lock = threading.Lock()
+
+
+def mine_tweet_data(hashtag: str, time_str):
+    
+    file_name = hashtag + "_" + str(time_str) + ".json"
+    json_file_name =  os.path.join(JSON_DIR, file_name)
     # Search for latest tweets about "#nlproc"
-    twitter.search.tweets(q='#bitcoin')
-    sfo_trends = twitter.search.tweets(q='#bitcoin', result_type='recent', lang='en', count=300)
-    file = open(file_name, "a+")
+    sfo_trends = twitter.search.tweets(q=hashtag, result_type='recent', lang='en', count=300)
+    file = open(json_file_name, "a+")
     file.write(json.dumps(sfo_trends, indent=10))
     file.close()
-    count += 1
-    print("File Number", count, "created. Name:", file_name.split('/')[-1])
-    time.sleep(15)
 
-    file = open("tmp/tweets.json", 'a+')
+    tweet_file = open(os.path.join(os.path.join(TWEET_DIR, hashtag), file_name), 'a+')
 
-    json_data = json.load(open(file_name, 'r'))
+    json_data = json.load(open(json_file_name, 'r'))
 
     for index, tweet in enumerate(json_data['statuses']):
-        jsonParser = JsonTweetParser(json_data['statuses'][index], time_str=timestr)
-        file.write(str(jsonParser.construct_tweet_json()) + ",\n")
+        jsonParser = JsonTweetParser(json_data['statuses'][index], time_str=time_str)
+        tweet_file.write(str(jsonParser.construct_tweet_json()) + ",\n")
 
-    file.close()
+    tweet_file.close()
+    
+    with print_lock:
+        print(threading.current_thread().name, hashtag)
 
-    if count == 100:
-        print("Process Finished")
-        exit(1)
+def threader():
+    while True:
+        
+        hashtag = q.get()
+        time_str = time.strftime("%Y-%m-%d_%H-%M-%S")
+        
+        if hashtag is None:
+            break
+        
+        mine_tweet_data(hashtag, time_str)
+        q.task_done()
+
+
+if os.path.isfile("mkdirectories.py"):
+    os.system("./mkdirectories.py")
+else:
+    import sys
+    print("\033[31mERROR: mkdirectories.py is not in the current directory.\033[0m",
+         file=sys.stderr)
+    exit(-1)
+
+
+count = 1
+start = time.time()
+while True:
+
+    iteration_start = time.time()
+    q = Queue()
+
+    # Initialize Threads
+    threads = []
+    for x in range(NUM_THREADS):
+        t = threading.Thread(target=threader)
+        t.daemon = True  
+        t.name = x
+        threads.append(t)
+        t.start()
+        
+    for worker in hashtags:
+        q.put(worker)
+
+    q.join()
+    
+    # stop workers
+    for i in range(NUM_THREADS):
+        q.put(None)
+    
+    for t in threads:
+        t.join()
+
+    print("Iteration", count, "Complete. Time Elapsed: {}".format(time.time() - start))
+    
+    #Wait at least until the designated number of seconds allocated for each iteration has passed
+    while True:
+        if (time.time() - iteration_start) >= SECONDS_PER_ITERATION:
+            break
+
+    if count == 3:
+        break
+    
+    count += 1
+
+print("Entire Job Took: {}".format(time.time() - start))
