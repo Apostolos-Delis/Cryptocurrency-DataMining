d69c8c86ffb16839845610a15dac6e43d0f309f4
Apostolos-Delis <apost.delis@gmail.com>
2018-12-31 15:32:27 -0800

    removed pull_tweets.py

--- src/data_collection/__init__.py
+++ src/data_collection/__init__.py
@@ -2,3 +2,5 @@
 # coding: utf8
 from .tweet_manager import TweetManager
 from .cryptocurrency import Cryptocurrency
+from .utilities import error, clean_text_for_tfidf, make_directory
+from .utilities import text_sentiment
