c1fdd2f32f2abdd9f64defee362a8d256f34e744
Apostolos-Delis <apost.delis@gmail.com>
2018-12-30 17:50:55 -0800

    cryptocurrency class will define all coin objects

---
+++ src/cryptocurrency.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+
+import os
+import pickle
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import quandl
+
+
+class Cryptocurrency:
+
+    def __init__(self, name: str, ticker: str, 
+            date_founded: str, exchanges: list):
+        self.name = name
+        self.ticker = ticker.upper()
+        self.date_founded = date_founded
+        self.exchanges = exchanges
+
+
+    def __str__(self):
+        return (f"Cryptocurrency: {self.name} " 
+                f"({self.ticker}) at location <{hex(id(self))}>")
+
+
+    def get_market_data(self):
+        exchange_data = {}
+
+        btc_usd_price_kraken = get_quandl_data('BCHARTS/KRAKENUSD')
+        exchange_data['KRAKEN'] = btc_usd_price_kraken
+
+        for exchange in exchanges:
+            exchange_code = 'BCHARTS/{}USD'.format(exchange)
+            btc_exchange_df = get_quandl_data(exchange_code)
+            exchange_data[exchange] = btc_exchange_df  
+
+        return exchange_data
+        
+
+
+def get_quandl_data(quandl_id):
+    """Download and cache Quandl dataseries"""
+    cache_path = '{}.pkl'.format(quandl_id).replace('/','-')
+    try:
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(quandl_id))
+    except (OSError, IOError) as e:
+        print('Downloading {} from Quandl'.format(quandl_id))
+        df = quandl.get(quandl_id, returns="pandas")
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(quandl_id, cache_path))
+    return df
+
+
+if __name__ == "__main__":
+    btc = Cryptocurrency("bitcoin", "btc", "2008-10-31", ["COINBASE"])
+    print(btc)
+    print(btc.get_market_data())
+
